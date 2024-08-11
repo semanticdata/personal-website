4818c64c52c420d6426dc8a62697bd4798077daa
Miguel Pimentel <contact@miguelpimentel.do>
2024-08-10 22:12:51 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://semanticdata.github.io",
-  base: "astro-micro",
+  base: "/astro-micro",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
