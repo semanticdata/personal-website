51f8d9599bdfe16245ef1ce5b578e7573b493c41
semanticdata <contact@miguelpimentel.do>
2024-08-11 20:30:28 -0500

    adjust number of posts and projects

--- src/consts.ts
+++ src/consts.ts
@@ -4,8 +4,8 @@ export const SITE: Site = {
   TITLE: "Miguel Pimentel",
   DESCRIPTION: "Problem solver, hobby developer, music enjoyer, and public infrastructure enthusiast based in Minnesota, US.",
   EMAIL: "contact@miguelpimentel.com",
-  NUM_POSTS_ON_HOMEPAGE: 7,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 5,
 };
 
 export const HOME: Metadata = {
