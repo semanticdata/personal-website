8b96d5c4e29f741b41f17738847e87b908c097c2
semanticdata <contact@miguelpimentel.do>
2024-08-11 20:29:11 -0500

    update new url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-micro-opal.vercel.app/",
+  site: "https://miguelpimentel.do/",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
